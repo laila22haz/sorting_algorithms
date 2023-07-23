87b2d154648b2556d0e28402b92cee485ccf2988
laila22haz <hazmirlaila316@gmail.com>
2023-07-23 08:28:41 +0100

    selection third update output

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -10,20 +10,26 @@ void selection_sort(int *array, size_t size)
 {
 	int min, temp;
 	size_t i, j;
+	int flag;
 
 	if (size < 2 || array == NULL)
-	return;
+		return;
 	for (i = 0; i < size - 1; i++)
 	{
+		flag = 0;
 		min = i;
-		for (j = i + 1; j < size + 1; j++)
+		for (j = i + 1; j < size; j++)
 		{
 			if (array[j] < array[min])
-			min = j;
+			{
+				min = j;
+				flag = 1;
+			}
 		}
+	temp = array[min];
+	array[min] = array[i];
+	array[i] = temp;
+	if (flag)
 		print_array(array, size);
-		temp = array[min];
-		array[min] = array[i];
-		array[i] = temp;
 	}
 }
